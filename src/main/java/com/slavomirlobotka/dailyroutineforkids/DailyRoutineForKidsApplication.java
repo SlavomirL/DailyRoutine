a0e71bcf6d12b6bf08b9337c91cba481410333fd
Slavomir Lobotka <slavomir.lobotka@yahoo.com>
2024-04-17 21:13:08 +0200

    initial commit

---
+++ src/main/java/com/slavomirlobotka/dailyroutineforkids/DailyRoutineForKidsApplication.java
@@ -0,0 +1,13 @@
+package com.slavomirlobotka.dailyroutineforkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyRoutineForKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyRoutineForKidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slavomirlobotka/dailyroutineforkids/DailyRoutineForKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavomirlobotka.dailyroutineforkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRoutineForKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
