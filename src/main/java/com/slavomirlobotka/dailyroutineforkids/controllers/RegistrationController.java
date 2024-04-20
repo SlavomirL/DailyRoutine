43c145bcaae5d4828a23a9823b66e3a306a04d7e
Slavomir Lobotka <slavomir.lobotka@yahoo.com>
2024-04-20 12:29:15 +0200

    refactor: remove commented code

--- src/main/java/com/slavomirlobotka/dailyroutineforkids/controllers/RegistrationController.java
+++ src/main/java/com/slavomirlobotka/dailyroutineforkids/controllers/RegistrationController.java
@@ -19,7 +19,6 @@ public class RegistrationController {
   public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
     registrationService.registerNewParent(registerRequestDTO);
 
-//    return ResponseEntity.ok("Parent " + registerRequestDTO.getFirstName() + " " + registerRequestDTO.getSurname() + " is now registered");
     return ResponseEntity.ok(String.format("Parent %s %s is now successfully registered.", registerRequestDTO.getFirstName(), registerRequestDTO.getSurname()));
   }
 }
