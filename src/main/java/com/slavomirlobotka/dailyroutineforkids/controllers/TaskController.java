39454a93c59ce4c5232e40d8a9107f510a8dadfc
Slavomir Lobotka <slavomir.lobotka@yahoo.com>
2024-05-25 12:37:14 +0200

    feat: tasks name and description can be updated

--- src/main/java/com/slavomirlobotka/dailyroutineforkids/controllers/TaskController.java
+++ src/main/java/com/slavomirlobotka/dailyroutineforkids/controllers/TaskController.java
@@ -1,8 +1,10 @@
 package com.slavomirlobotka.dailyroutineforkids.controllers;
 
+import com.slavomirlobotka.dailyroutineforkids.dtos.UpdateTaskDTO;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineBadRequest;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineNotFound;
 import com.slavomirlobotka.dailyroutineforkids.models.ScheduleTask;
+import com.slavomirlobotka.dailyroutineforkids.models.Task;
 import com.slavomirlobotka.dailyroutineforkids.services.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -26,5 +28,26 @@ public class TaskController {
     }
 
     return ResponseEntity.ok("Task '" + taskName + "' created.");
+  }
+
+  @PatchMapping("/tasks/{taskId}")
+  public ResponseEntity<?> modifyTask(
+      @PathVariable Long taskId, @RequestBody(required = false) UpdateTaskDTO updateTaskDTO)
+      throws DailyRoutineNotFound, DailyRoutineBadRequest {
+    Task task = taskService.updateTask(taskId, updateTaskDTO);
+    if (updateTaskDTO.getDescription() != null && updateTaskDTO.getTaskName() != null) {
+      return ResponseEntity.ok(
+          "Task updated. New name: '"
+              + task.getTaskName()
+              + "'. New description: '"
+              + task.getDescription()
+              + "'.");
+    } else if (updateTaskDTO.getDescription() != null) {
+      return ResponseEntity.ok("Task updated. New description: '" + task.getDescription() + "'.");
+    } else if (updateTaskDTO.getTaskName() != null) {
+      return ResponseEntity.ok("Task updated. New name: '" + task.getTaskName() + "'.");
+    } else {
+      return ResponseEntity.ok("No changes for the task '" + task.getTaskName() + "'.");
+    }
   }
 }
--- src/main/java/com/slavomirlobotka/dailyroutineforkids/services/TaskServiceImpl.java
+++ src/main/java/com/slavomirlobotka/dailyroutineforkids/services/TaskServiceImpl.java
@@ -1,6 +1,8 @@
 package com.slavomirlobotka.dailyroutineforkids.services;
 
+import com.slavomirlobotka.dailyroutineforkids.dtos.UpdateTaskDTO;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineBadRequest;
+import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineNotFound;
 import com.slavomirlobotka.dailyroutineforkids.models.Task;
 import com.slavomirlobotka.dailyroutineforkids.repositories.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +17,7 @@ public class TaskServiceImpl implements TaskService {
   @Override
   public void createCustomTask(String taskName) throws DailyRoutineBadRequest {
     if (!taskRepository.existsByTaskName(taskName)) {
-      Task newTask = Task.builder().taskName(taskName).build();
+      Task newTask = Task.builder().taskName(taskName).custom(true).build();
       taskRepository.save(newTask);
     } else {
       throw new DailyRoutineBadRequest("Task with name '" + taskName + "' already exists.");
@@ -25,10 +27,34 @@ public class TaskServiceImpl implements TaskService {
   @Override
   public void createCustomTask(String taskName, String description) throws DailyRoutineBadRequest {
     if (!taskRepository.existsByTaskName(taskName)) {
-      Task newTask = Task.builder().taskName(taskName).description(description).build();
+      Task newTask =
+          Task.builder().taskName(taskName).custom(true).description(description).build();
       taskRepository.save(newTask);
     } else {
       throw new DailyRoutineBadRequest("Task with name '" + taskName + "' already exists.");
     }
+  }
+
+  @Override
+  public Task updateTask(Long taskId, UpdateTaskDTO updateTaskDTO)
+      throws DailyRoutineNotFound, DailyRoutineBadRequest {
+    Task task =
+        taskRepository
+            .findById(taskId)
+            .orElseThrow(() -> new DailyRoutineNotFound("No task with ID '" + taskId + "' found."));
+
+    if(!task.getCustom()) {
+      throw new DailyRoutineBadRequest("Default Tasks cannot be modified");
+    }
+
+    if (updateTaskDTO.getTaskName() != null) {
+      task.setTaskName(updateTaskDTO.getTaskName());
+    }
+    if (updateTaskDTO.getDescription() != null) {
+      task.setDescription(updateTaskDTO.getDescription());
+    }
+    taskRepository.save(task);
+
+    return task;
   }
 }
