8318a27efe1919803736e6c472a746b2e7d0e34e
Slavomir Lobotka <slavomir.lobotka@yahoo.com>
2024-06-01 09:03:24 +0200

    fix: email for registration has to be unique. error handling during login with incorrect credentials

--- src/main/java/com/slavomirlobotka/dailyroutineforkids/services/AuthenticationService.java
+++ src/main/java/com/slavomirlobotka/dailyroutineforkids/services/AuthenticationService.java
@@ -4,13 +4,15 @@ import com.slavomirlobotka.dailyroutineforkids.dtos.AuthenticationResponseDTO;
 import com.slavomirlobotka.dailyroutineforkids.dtos.LoginRequestDTO;
 import com.slavomirlobotka.dailyroutineforkids.dtos.RegisterRequestDTO;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineBadRequest;
+import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineForbidden;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineIO;
 import com.slavomirlobotka.dailyroutineforkids.exceptions.DailyRoutineUnauthorized;
 import com.slavomirlobotka.dailyroutineforkids.models.User;
 
 public interface AuthenticationService {
 
-  void registerNewParent(RegisterRequestDTO registerRequestDTO) throws DailyRoutineIO;
+  void registerNewParent(RegisterRequestDTO registerRequestDTO)
+      throws DailyRoutineIO, DailyRoutineForbidden;
 
   String generateAndSaveActivationToken(User user);
 
@@ -19,9 +21,11 @@ public interface AuthenticationService {
   boolean verifyUserCode(String code, String email) throws DailyRoutineBadRequest;
 
   AuthenticationResponseDTO authenticate(LoginRequestDTO loginRequestDTO)
-      throws DailyRoutineUnauthorized;
+      throws DailyRoutineUnauthorized, DailyRoutineBadRequest;
 
   boolean isUserEnabled(String email);
 
-    User getCurrentParent();
+  User getCurrentParent();
+
+  boolean isEmailAlreadyUsed(String email);
 }
